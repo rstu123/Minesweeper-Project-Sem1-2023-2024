964fe068071d9b31b26a3e3a5c1de5a8b676e25d
nghianguyen19 <117875565+nghianguyen19@users.noreply.github.com>
2023-12-23 01:42:39 +0700

    Update Board.java

--- src/cnf/dsaproject/minesweeper/game/Board.java
+++ src/cnf/dsaproject/minesweeper/game/Board.java
@@ -16,4 +16,19 @@ public class Board {
 	private int NMines;
 	private int NCovered;
 	private GameStates Gamestate;
+	
+	// save data in separate arrays, instead of an array of objects
+	// this increases CPU-Register cache hit so can run faster
+	private boolean[][] isMine;
+	private int[][] mineCnt;
+	private CellStates[][] states;
+
+	// used in putMines() and bfs()
+	private final int[] di = new int[] { -1, -1, -1, 0, 1, 1, 1, 0 };
+	private final int[] dj = new int[] { -1, 0, 1, 1, 1, 0, -1, -1 };
+
+	private final CellStates[] uncoveredStates = new CellStates[] { 
+			CellStates.UNC0, CellStates.UNC1, CellStates.UNC2, CellStates.UNC3, 
+			CellStates.UNC4, CellStates.UNC5, CellStates.UNC6, CellStates.UNC7, CellStates.UNC8 
+	};
 }
