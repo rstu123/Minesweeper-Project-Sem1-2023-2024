53df62f312768883a51a82822b148ceaa9003fd1
ITITIU21148 <118949870+ITITIU21148@users.noreply.github.com>
2023-12-25 20:39:03 +0700

    Update MouseManager.java

--- src/cnf/dsaproject/minesweeper/game/MouseManager.java
+++ src/cnf/dsaproject/minesweeper/game/MouseManager.java
@@ -1,5 +1,28 @@
 package cnf.dsaproject.minesweeper.game;
 
-public class MouseManager{
-    
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseManager implements MouseListener {
+    private Game game;
+
+	public MouseManager(Game game) {
+		this.game = game;
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		boolean isLeft = (e.getButton() == MouseEvent.BUTTON1);
+		if (game != null)
+			game.onClick(isLeft, e.getX(), e.getY());
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {}
+	@Override
+	public void mousePressed(MouseEvent e) {}
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+	@Override
+	public void mouseExited(MouseEvent e) {}
 }
