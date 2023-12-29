1dcdd38ad2613522d3dd717dd32b23378aab3e37
ITITIU21148 <118949870+ITITIU21148@users.noreply.github.com>
2023-12-29 22:51:04 +0700

    Update Launcher.java

--- src/cnf/dsaproject/minesweeper/game/Launcher.java
+++ src/cnf/dsaproject/minesweeper/game/Launcher.java
@@ -2,6 +2,30 @@ package cnf.dsaproject.minesweeper.game;
 
 public class Launcher {
     public static void main(String[] args){
+    int N = 20, NMines = 50;
 
-    }
+	if (args.length > 0) {
+		try {
+			N = Integer.parseInt(args[0]);
+			NMines = Integer.parseInt(args[1]);
+		} 
+        catch (Exception ex) {
+			System.out.println("Invalid parameters!");
+			System.out.println("Should be: java -jar Minesweeper.jar [Board_width Number_of_Mines]");
+			System.exit(-1);
+		}
+	}
+		
+	if (N < 10 || N > 1000 || NMines < 1 || NMines > N * N) {
+        System.out.println("Parameters are not reasonable numbers!");
+        System.out.println("Should be: java -jar Minesweeper.jar [Board_width Number_of_Mines]");
+        System.out.println("\tBoard_width: \t[10, 1000], otherwise your screen won't fit");
+        System.out.println("\tNumber_of_Mines: \t[1, Board_width^2]");
+        System.exit(-1);
+	}
+
+	Game game = new Game("Minesweeper", N, NMines);
+	game.start();
+	}
+
 }
